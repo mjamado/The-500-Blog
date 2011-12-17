38ac2c7548c3f1ee12f409d60c730c985e55dc38
Marco Amado <mjamado@dreamsincode.com>
2011-12-17 01:22:37 +0000

    Update controllers/UserAdminController.class.php

--- controllers/UserAdminController.class.php
+++ controllers/UserAdminController.class.php
@@ -1,66 +1,43 @@
-<?php
-class UserAdminController
-{
+<?php class UserAdminController {
 	private $title = "Users";
-
-	public function Index()
-	{
+	public function Index() {
 		$page = empty($_GET['page']) ? 1 : $_GET['page'];
 		$orderBy = empty($_GET['orderby']) ? 'registration' : $_GET['orderby'];
 		$orderDir = empty($_GET['orderdir']) ? 'DESC' : $_GET['orderdir'];
-
 		$obj = new User();
 		$items = $obj->GetAll(null, array($orderBy, $orderDir), array('page' => $page, 'numItemsPage' => 20));
 		$paginator = $items->paginator;
-
 		$view = 'adminUser/index.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Edit()
-	{
+	public function Edit() {
 		$id = empty($_GET['id']) ? 0 : $_GET['id'];
-
 		$user = new User($id);
-
-		if(!empty($_POST['user']) && is_array($_POST['user']))
-		{
+		if(!empty($_POST['user']) && is_array($_POST['user'])) {
 			$user->data = $_POST['user'];
 			$user->Save();
-
 			header('Location: /admin/User/');
 			exit();
 		}
-
 		$view = 'adminUser/details.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Add()
-	{
-		if(!empty($_POST['user']) && is_array($_POST['user']))
-		{
+	public function Add() {
+		if(!empty($_POST['user']) && is_array($_POST['user'])) {
 			$user = new User();
 			$user->data = $_POST['user'];
 			$user->Save();
-
 			header('Location: /admin/User/');
 			exit();
 		}
-
 		$view = 'adminUser/details.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Delete()
-	{
+	public function Delete() {
 		$id = empty($_GET['id']) ? 0 : $_GET['id'];
-
 		$user = new User($id);
 		$user->ToDelete();
-
 		header('Location: /admin/User/');
 		exit();
 	}
-}
-?>
+} ?>

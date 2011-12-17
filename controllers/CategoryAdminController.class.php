e4aed027228efd6ad99e0dbe8ac4e81ffbd578ba
Marco Amado <mjamado@dreamsincode.com>
2011-12-17 00:35:35 +0000

    Update controllers/CategoryAdminController.class.php

--- controllers/CategoryAdminController.class.php
+++ controllers/CategoryAdminController.class.php
@@ -1,66 +1,44 @@
-<?php
-class CategoryAdminController
+<?php class CategoryAdminController
 {
 	private $title = "Categories";
-
-	public function Index()
-	{
+	public function Index() {
 		$page = empty($_GET['page']) ? 1 : $_GET['page'];
 		$orderBy = empty($_GET['orderby']) ? 'posted' : $_GET['orderby'];
 		$orderDir = empty($_GET['orderdir']) ? 'DESC' : $_GET['orderdir'];
-
 		$obj = new Category();
 		$items = $obj->GetAll(null, array($orderBy, $orderDir), array('page' => $page, 'numItemsPage' => 20));
 		$paginator = $items->paginator;
-
 		$view = 'adminCategory/index.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Edit()
-	{
+	public function Edit() {
 		$id = empty($_GET['id']) ? 0 : $_GET['id'];
-
 		$category = new Category($id);
-
-		if(!empty($_POST['category']) && is_array($_POST['category']))
-		{
+		if(!empty($_POST['category']) && is_array($_POST['category'])) {
 			$category->data = $_POST['category'];
 			$category->Save();
-
 			header('Location: /admin/Category/');
 			exit();
 		}
-
 		$view = 'adminCategory/details.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Add()
-	{
-		if(!empty($_POST['category']) && is_array($_POST['category']))
-		{
+	public function Add() {
+		if(!empty($_POST['category']) && is_array($_POST['category'])) {
 			$category = new Category();
 			$category->data = $_POST['category'];
 			$category->Save();
-
 			header('Location: /admin/Category/');
 			exit();
 		}
-
 		$view = 'adminCategory/details.php';
 		require_once('default/adminMaster.php');
 	}
-
-	public function Delete()
-	{
+	public function Delete() {
 		$id = empty($_GET['id']) ? 0 : $_GET['id'];
-
 		$category = new Category($id);
 		$category->ToDelete();
-
 		header('Location: /admin/Category/');
 		exit();
 	}
-}
-?>
+} ?>
